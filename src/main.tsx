3edb7e4028c0e4c140a4675c7694a52c5a65c269
kamge80-svg <KAMGE80@GMAIL.COM>
2025-11-20 00:33:34 +0100

    Disable service worker for Vercel deployment

--- src/main.tsx
+++ src/main.tsx
@@ -7,13 +7,13 @@ import { NotificationProvider } from './contexts/NotificationContext';
 import { ThemeProvider } from './contexts/ThemeContext';
 import { LanguageProvider } from './contexts/LanguageContext';
 import { ErrorBoundary } from './components/ErrorBoundary';
-import { registerServiceWorker, checkInstallPrompt } from './utils/pwa';
+// import { registerServiceWorker, checkInstallPrompt } from './utils/pwa';
 import App from './App.tsx';
 import './index.css';
 
-// Register PWA
-registerServiceWorker();
-checkInstallPrompt();
+// Register PWA - Désactivé temporairement pour Vercel
+// registerServiceWorker();
+// checkInstallPrompt();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
